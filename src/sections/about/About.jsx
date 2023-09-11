ebd42d41cc500e93a5ef8a52028645bdda94bf85
Reddi Manikanta Surya Prasad <reddimani14@gmail.com>
2023-09-11 22:09:51 +0530

    commit

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
                 Building projects my clients love have always been my passion. Being in the web development industry for over a year, I'm always motivated to do more!
                 </p>
                 <p>
-                Hi, my name is Reddi Manikanta Surya Prasad from Andhra Pradesh, India. I'm a full-stack web developer with a Bachelors degree in Computer Science. My top priority is to get your business online the right way, giving you industry-standard design and all the functionality you need to operate smoothly online. Get in touch today with the details of your project let's get started! Check out my resume below!
+                Hi, my name is Reddi Manikanta Surya Prasad from Andhra Pradesh, India. I'm a full-stack web developer and also a DevOps Engineer with a Bachelors degree in Computer Science. My top priority is to get your business online the right way, giving you industry-standard design and all the functionality you need to operate smoothly online. Get in touch today with the details of your project let's get started! Check out my resume below!
                 </p>
                 <a href={CV} download className='btn primary'>Download CV <HiDownload/></a>
             </div>
